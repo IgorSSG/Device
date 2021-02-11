cb67f2289f6008b5a90322fe1a064ebfd2cc337e
OTC-NB2\USR <rysigr001@gmail.com>
2021-02-11 17:55:01 +0300

    ок

---
+++ JS/script.js
@@ -0,0 +1,12 @@
+let promo_items = document.querySelectorAll('.promo-slider__item');
+console.log(promo_items);
+
+let promo_buttons = document.querySelectorAll('.promo-slider__input')
+console.log(promo_buttons);
+
+
+for (i = 0; i < promo_buttons.length; i++) {
+  promo_buttons[i].oncklick = function slider(e) {
+
+  }
+}
